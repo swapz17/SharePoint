ffc40a7bbe1c83b6ba431561cdd9e95442de8054
Swapnil Kamble <swapnil.kamble@outlook.com>
2020-08-22 00:21:22 +0530

    Basic code
    Get web title

--- SPOConsole/SPOConsole/Program.cs
+++ SPOConsole/SPOConsole/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
 
 namespace SPOConsole
 {
@@ -10,6 +13,25 @@ namespace SPOConsole
     {
         static void Main(string[] args)
         {
+            string URL = "";
+            string username = ConfigurationManager.AppSettings.Get("username");
+            string passtext = ConfigurationManager.AppSettings.Get("password");
+            var password = new SecureString();
+            foreach(var ch in passtext)
+            {
+                password.AppendChar(ch);
+            }
+
+            using (var context = new ClientContext(URL)) 
+            {
+                context.Credentials = new SharePointOnlineCredentials(username, password);
+                Web web = context.Web;
+                context.Load(web, w => w.Title);
+                context.ExecuteQuery();
+                Console.WriteLine(web.Title);
+            }
+
+            Console.ReadKey();
         }
     }
 }
